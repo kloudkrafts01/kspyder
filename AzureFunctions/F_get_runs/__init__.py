6f94fdd9f835dc9e491ab811d40cbc3240757fe6
alexandre.mougel@bonnegueule.fr <alexandre.mougel@bonnegueule.fr>
2021-04-08 11:50:50 +0200

    enhanced error message return for F_get_runs

--- AzureFunctions/F_get_runs/__init__.py
+++ AzureFunctions/F_get_runs/__init__.py
@@ -12,14 +12,26 @@ async def main(req: func.HttpRequest, starter: str) -> func.HttpResponse:
     instances = await client.get_status_all()
     instances_list = []
 
-    for instance in instances:
-        instances_list += instance.to_json(),
-        logger.info(json.dumps(instance))
+    try: 
+        for instance in instances:
+            instances_list += instance.to_json(),
+            logger.info(json.dumps(instance))
+        resp_body = json.dumps(instances_list)
 
-    response = func.HttpResponse(
-        body = json.dumps(instances_list),
-        status_code = 200,
-        mimetype = 'application/json'
-    )
+        response = func.HttpResponse(
+            body = resp_body,
+            status_code = 200,
+            mimetype = 'application/json'
+        )
+
+    except Exception as e:
+        resp_body = json.dumps({
+            'error': e
+        }) 
+        response = func.HttpResponse(
+            body = resp_body,
+            status_code = 200,
+            mimetype = 'application/json'
+        )
 
     return response
