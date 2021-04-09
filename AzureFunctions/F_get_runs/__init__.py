4744f7244438d0eb6734f19cdf06cda205f513fc
alexandre.mougel@bonnegueule.fr <alexandre.mougel@bonnegueule.fr>
2021-04-09 12:00:34 +0200

    simplified get_runs function

--- AzureFunctions/F_get_runs/__init__.py
+++ AzureFunctions/F_get_runs/__init__.py
@@ -6,28 +6,28 @@ import azure.durable_functions as df
 from common.spLogging import logger
 
 
-async def main(req: func.HttpRequest, starter: str) -> func.HttpResponse:
+async def main(req: func.HttpRequest, starter: str) -> None:
     client = df.DurableOrchestrationClient(starter)
 
     instances = await client.get_status_all()
     instances_list = []
 
-    response = None
+    # response = None
 
     try: 
         for instance in instances:
-            instances_list += instance.to_json(),
+            # instances_list += instance.to_json(),
             logger.info(json.dumps(instance))
-        resp_body = json.dumps(instances_list)
+        # resp_body = json.dumps(instances_list)
 
-        response = func.HttpResponse(
-            body = resp_body,
-            status_code = 200,
-            mimetype = 'application/json'
-        )
+        # response = func.HttpResponse(
+        #     body = resp_body,
+        #     status_code = 200,
+        #     mimetype = 'application/json'
+        # )
 
     except Exception as e:
         
         logger.error("F_get_runs :: {}".format(e))
 
-    return response
+    # return response
