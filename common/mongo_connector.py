d417083240aca5acfbdb2a9305d5cdea7786a395
Alexandre Mougel <alex@kloudkrafts.io>
2021-09-02 11:00:43 +0200

    Added CSV outputs for mongo aggregation configs

--- common/mongo_connector.py
+++ common/mongo_connector.py
@@ -1,19 +1,20 @@
 from sqlalchemy.orm import query_expression
-from Connectors.azureResourceGraph import read_query
+from Connectors.azureResourceGraph import SCHEMA_NAME, read_query
 import json
 from pymongo import MongoClient
 
-from common.config import APP_NAME, DUMP_JSON, load_conf
-from common.utils import json_dump
+from common.config import APP_NAME, DUMP_JSON, DUMP_CSV, load_conf
+from common.utils import json_dump, csv_dump
 from common.spLogging import logger
 
 MONGO_QUERIES = load_conf("mongodb_queries",subfolder="manifests")
+SCHEMA_NAME = APP_NAME
 
 class MongoDBConnector():
 
     def __init__(self):
         self.client = MongoClient('localhost',27017)
-        self.db = self.client[APP_NAME]
+        self.db = self.client[SCHEMA_NAME]
 
     def insert_dataset(self,dataset):
 
@@ -40,7 +41,6 @@ class MongoDBConnector():
     def execute_queries(self, query_names=None):
         
         queries = {}
-        
         #extracting a subset of the MONGO_QUERIES dicitonary if query names were explicitly provided
         if query_names:
             queries = {query_name: MONGO_QUERIES[query_name] for query_name in set(query_names)}
@@ -49,25 +49,38 @@ class MongoDBConnector():
 
         for query_name,query_conf in queries.items():
             logger.info("Preparing Mongo Query {}: {}".format(query_name,query_conf))
-            results = self.read_query(query_conf)
-            results_list = list(results)
-
-            logger.debug(results_list)
+            result_dataset = self.execute_query(query_name,query_conf)
 
             if DUMP_JSON:
-                json_dump(results_list,APP_NAME,query_name)
-    
-    def read_query(self,query_conf):
+                json_dump(result_dataset,APP_NAME,query_name)
 
-        results_list = []
+            if query_conf['dump_csv']:
+                csv_dump(result_dataset['data'],APP_NAME,query_name)
+
+    def execute_query(self,query_name,query_conf):
+
         model_name, queryPipeline = build_mongo_query(query_conf)
         collection = self.db[model_name]
 
         logger.info("Executing mongo Query on Collection {}: {}".format(model_name,queryPipeline))
 
         results = collection.aggregate(queryPipeline)
+        results_list = list(results)
 
-        return results
+        logger.debug(results_list)
+
+        result_dataset = {
+            "header": {
+                "schema": SCHEMA_NAME,
+                "model": model_name,
+                "query_name": query_name,
+                "query_conf": query_conf,
+                "count": len(results_list),
+            },
+            "data": results_list
+        }
+
+        return result_dataset
 
 def build_mongo_query(query_conf):
 
