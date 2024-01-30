40702d613d2d089666b52f32f535993eaa70ec88
Alexandre Mougel <alex@kloudkrafts.io>
2024-01-30 01:44:08 +0100

    testable aliyunCLIConnector

--- Connectors/aliyunCLIConnector.py
+++ Connectors/aliyunCLIConnector.py
@@ -5,11 +5,10 @@ import subprocess
 from common.extract import GenericExtractor
 from common.spLogging import logger
 
-from common.config import ODOO_PROFILE, PAGE_SIZE, BASE_FILE_HANDLER as fh
+from common.config import PAGE_SIZE, BASE_FILE_HANDLER as fh
 
 
 # Load the Connector's config
-# CONF = load_conf('odoo_models', subfolder='manifests')
 CONF = fh.load_yaml('aliyunCLIModels', subfolder=__name__)
 CONNECTOR_CONF = CONF['Connector']
 SCHEMA_NAME = CONNECTOR_CONF['schema']
@@ -28,10 +27,10 @@ class aliyunCLIClient:
     
     def build_command(self,model,scope,search_domains=[]):
 
-        command_str = ''
+        command = ['aliyun', model['class']]
         # build the basics : 'scope' is supposed to be one type of query that fits the model
         if scope in model['scopes']:
-            command_str = 'aliyun {} {} '.format(model['class'], scope)
+            command += scope,
         else:
             errmsg_tmpl = '{} :: {} is not a valid scope for the {} model.\nAccepted scopes are: {}'
             errmsg = errmsg_tmpl.format(__name__, scope, model, model['scopes'])
@@ -40,98 +39,110 @@ class aliyunCLIClient:
 
         # add search filters if any
         for domain in search_domains:
+            # if there was a given start_time, the syntax is a bit different
             if domain[0] == self.update_field:
-                last_time = 
-                timefilter = '--Filter.1.Key {} --Filter.1.Value {} '.format(self.update_field, last_time)
-                command_str += timefilter
-            command_str += '--{} {} '.format(domain[0], domain[2])
+                timefilter = [
+                    '--Filter.1.Key',
+                    domain[0],
+                    '--Filter.1.Value',
+                    domain[2]
+                ]
+                command = command + timefilter
+            else:
+                command = command + [f'--{domain[0]}', domain[2]]
         
+        logger.debug('{} :: Aliyun command: {}'.format(__name__, command))
 
+        return command
 
-        return command_str
-
-
-
-
-    def get_records_count(self,model,search_domains=[]):
+    def get_records_count(self,model,scope,search_domains=[]):
         
-        result = self.models.execute_kw(
-            self.dbname, self.uid, self.__password,
-            model['odoo_name'], 'search_count',
-            [search_domains]
-        )
-        return result
+        command = self.build_command(model,scope,search_domains)
+        result = subprocess.run(command)
+        return result['TotalCount']
 
-    def search_read(self,model,search_domains=[],offset=None,limit=PAGE_SIZE):
+    def search_read(self,model,scope,search_domains=[],offset=None,limit=PAGE_SIZE):
 
         fields = model['fields']
-        paramsDict = {'fields': fields, 'order': 'id'}
+        pkeys = [x for x in fields.keys() if 'primary_key' in fields[x].keys()]
+        paramsDict = {'fields': fields, 'order': pkeys[0]}
 
-        if offset:
-            paramsDict['offset'] = offset
+        add_params = []
 
         if limit:
-            paramsDict['limit'] = limit
+            # for AliyunCLI pafge size is strictly limited to 100
+            capped_limit = limit if limit < 100 else 100
+            # paramsDict['limit'] = capped_limit
+            search_domains += ['--PageSize', '=', capped_limit],
 
-        result = self.models.execute_kw(
-            self.dbname, self.uid, self.__password,
-            model['odoo_name'], 'search_read',
-            [search_domains],
-            paramsDict
-        )
-        return result
+            if offset:
+                # paramsDict['offset'] = offset
+                # in AliyunCLI the offset is given as a page number
+                pageno = offset / capped_limit + 1
+                search_domains += ['--PageNumber', '=', pageno],
+
+        # Build the command and add up the offset and page size params
+        command = self.build_command(model,scope,search_domains=search_domains)
+        result = subprocess.run(command)
+
+        base_name = model['base_name']
+        basename_plural = base_name + 's'
+
+        dataset = result[basename_plural][base_name]
+
+        return dataset
 
 
 class aliyunCLIConnector(GenericExtractor):
 
-    def __init__(self, profile=ODOO_PROFILE, schema=SCHEMA_NAME, models=MODELS, update_field=UPD_FIELD_NAME,**params):
+    def __init__(self, schema=SCHEMA_NAME, models=MODELS, update_field=UPD_FIELD_NAME,**params):
 
-        # instantiate an Odoo XML-RPC client
-        self.client = aliyunCLIClient()
         self.schema = schema
         self.models = models
         self.update_field = update_field
+        self.client = aliyunCLIClient(update_field)
 
-    def get_count(self, model, search_domains=[]):
+    def get_count(self, model, scopes=None, search_domains=[]):
 
-        total_count = self.client.get_records_count(model,search_domains=search_domains)
+        total_count = 0
+        
+        for scope in scopes:
+            result = self.client.get_records_count(model,scope,search_domains)
+            total_count += result['TotalCount']
+        
         return total_count
 
-    def read_query(self,model,search_domains=[],start_row=0):
+    def read_query(self, model, scopes=None, search_domains=[], start_row=0):
 
-        results = self.client.search_read(model,search_domains=search_domains,offset=start_row)
+        if scopes is None:
+            scopes = model['scopes']
+
+        for scope in scopes:
+            results = self.client.search_read(model,scope,search_domains=search_domains,offset=start_row)
+            dataset = dataset + results
+
         return results
 
 
-    def forge_item(self,odoo_dict,model):
+    def forge_item(self,dictitem,model):
         '''function to split Odoo dict objects that contain two-value list as values, as it can happen when getting stuff from the Odoo RPC API.
         The values are split into two distinct fields, and if needed the second field can be dropped (e.g. when it contains PII we don't want to keep).'''
 
         new_dict = {}
         fields = model['fields']
 
-        for key,value in odoo_dict.items():
+        for key,value in dictitem.items():
 
             isInFieldMap = (key in fields)
-            isInUnpack = (key in UNPACKING)
 
-            if isInFieldMap and isInUnpack and value:
-
-                new_key = fields[key]['dbname']
-                new_dict[new_key] = value[0]
-                additional_field = UNPACKING[key]
-                
-                if additional_field is not None:
-                    additional_key = additional_field['dbname']
-                    new_dict[additional_key] = value[1]
-
-            elif isInFieldMap:
+            if isInFieldMap:
                 
                 new_key = fields[key]['dbname']
                 new_dict[new_key] = value
 
-            else:
-                new_dict[key] = value
+            # for now, filter out elements not in the model target fields
+            # else:
+            #     new_dict[key] = value
 
         return new_dict
 
